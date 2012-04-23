6364e749471ad718afc5e511e2c92fe45df500cc
zicjin <zicjin@gmail.com>
2012-04-23 15:39:49 +0800

    .

---
+++ Disposable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stackFluent.Data {
+    public class Disposable : IDisposable {
+        private bool isDisposed;
+
+        ~Disposable() {
+            Dispose(false);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing) {
+            if (!isDisposed && disposing) {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        protected virtual void DisposeCore() {
+        }
+    }
+}
---
+++ IUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+
+namespace stackFluent.Data {
+    /// <summary>
+    /// Represents the Unit Of Work.
+    /// </summary>
+    public interface IUnitOfWork {
+        /// <summary>
+        /// Gets a <see cref="System.Boolean"/> value which indicates whether the UnitOfWork
+        /// was committed.
+        /// </summary>
+        bool Committed {
+            get;
+        }
+        /// <summary>
+        /// Commits the UnitOfWork.
+        /// </summary>
+        void Commit();
+        /// <summary>
+        /// Rolls-back the UnitOfWork.
+        /// </summary>
+        void Rollback();
+    }
+}
