69a21073a6a588bc4cf6f4f3108212ee9d6a46fb
Cheney Jin <zicjin@gmail.com>
2012-04-27 03:24:41 +0800

    .

--- Disposable.cs
+++ Disposable.cs
@@ -5,25 +5,45 @@ using System.Text;
 
 namespace zic_dotnet {
     public class Disposable : IDisposable {
-        private bool isDisposed;
-
-        ~Disposable() {
-            Dispose(false);
-        }
 
         public void Dispose() {
-            Dispose(true);
+            ClearUp(true);
+            //使对象跳过Finalize()析构函数
             GC.SuppressFinalize(this);
         }
 
-        private void Dispose(bool disposing) {
-            if (!isDisposed && disposing) {
-                DisposeCore();
-            }
-            isDisposed = true;
+        //析构函数。使用override Finalize()无法编译
+        //析构函数是在本对象真正被回收时调用，Dispose()仅仅是一个用户回收资源的约定方法名
+        //如果对象未被调用Dispose()，则垃圾回收器会在回收时调用这个方法
+        ~Disposable() {
+            ClearUp(false);
         }
 
-        protected virtual void DisposeCore() {
+        private bool IsDisposed;
+
+        private void ClearUp(bool isManual) {
+            //报站只执行一次对象回收，防止用户调用多次Dispose()的资源消耗
+            if (!IsDisposed) {
+                if (isManual)
+                    DisposeHostobj();
+                DisposeDeHostobj();
+            }
+            IsDisposed = true;
         }
+
+        /// <summary>
+        /// 释放额外的托管资源，一般是本对象的内部字段
+        /// </summary>
+        protected virtual void DisposeHostobj() {
+
+        }
+
+        /// <summary>
+        /// 释放非托管资源
+        /// </summary>
+        protected virtual void DisposeDeHostobj() {
+
+        }
+
     }
 }
