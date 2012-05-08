fd1f9660b0cc494dcb4194f04decabc79acbc7db
Cheney Jin <zicjin@gmail.com>
2012-05-08 22:22:19 +0800

    .

--- Domain/IUnitOfWork.cs
+++ Domain/IUnitOfWork.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
 
 namespace zic_dotnet.Domain {
     /// <summary>
