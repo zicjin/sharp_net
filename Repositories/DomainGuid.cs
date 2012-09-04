2c907219f13273b477d8d6c2f64dd65674c10a06
zicjin <zicjin@gmail.com>
2012-09-04 20:10:11 +0800

    .

--- Repositories/DomainGuid.cs
+++ Repositories/DomainGuid.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace zic_dotnet.Repositories {
             get { return (eAct)ActEnum; }
             set { ActEnum = (int)value; }
         }
+
         public DateTime CreatTime { get; set; }
         public virtual void Creat() {
             ID = Guid.NewGuid();
--- Repositories/DomainInt.cs
+++ Repositories/DomainInt.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace zic_dotnet.Repositories {
             get { return (eAct)ActEnum; }
             set { ActEnum = (int)value; }
         }
+
         public DateTime CreatTime { get; set; }
         public virtual void Creat() {
             CreatTime = DateTime.Now;
