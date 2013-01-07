3c91d765c0fb443e99878547af1a3017f80dbaa0
cheney jin <zicjin@gmail.com>
2013-01-07 16:56:55 +0800

    remove ByteartRetail

--- Repositories/SortOrder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace sharp_net.Repositories {
-
-    public enum SortOrder {
-        Unspecified = -1,
-        Ascending = 0,
-        Descending = 1
-    }
-}
--- Specifications/Other/ISpecificationParser.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace sharp_net.Specifications {
-
-    public interface ISpecificationParser<TCriteria> {
-
-        TCriteria Parse<T>(ISpecification<T> specification);
-    }
-}
