48dd6dae3f75fa77ef3380277c3277a8b34f3d34
zicjin <zicjin@gmail.com>
2013-05-22 09:39:31 +0800

    .

---
+++ Mvc/ExternalLoginResult.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Web.WebPages.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace sharp_net.Mvc {
+    public class ExternalLoginResult : ActionResult {
+        public ExternalLoginResult(string provider, string returnUrl) {
+            Provider = provider;
+            ReturnUrl = returnUrl;
+        }
+
+        public string Provider { get; private set; }
+        public string ReturnUrl { get; private set; }
+
+        public override void ExecuteResult(ControllerContext context) {
+            OAuthWebSecurity.RequestAuthentication(Provider, ReturnUrl);
+        }
+    }
+}
