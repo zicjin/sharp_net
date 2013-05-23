6ce096274e271424b5263b06afa1f5301d2bd324
zicjin <zicjin@gmail.com>
2013-05-23 20:53:25 +0800

    .

---
+++ Mvc/JsonNetResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+namespace sharp_net.Mvc {
+    //https://gist.github.com/jpoehls/1424538
+
+    /// <summary>
+    /// A <see cref="JsonResult"/> implementation that uses JSON.NET to perform the serialization.
+    /// </summary>
+    public class JsonNetResult : JsonResult {
+        // public JsonRequestBehavior JsonRequestBehavior { get; set; }
+        // public Encoding ContentEncoding { get; set; }
+        // public string ContentType { get; set; }
+        // public object Data { get; set; }
+
+        public JsonSerializerSettings SerializerSettings { get; set; }
+        public Formatting Formatting { get; set; }
+
+        public JsonNetResult() {
+            Formatting = Formatting.None;
+            SerializerSettings = new JsonSerializerSettings();
+            JsonRequestBehavior = JsonRequestBehavior.DenyGet;
+        }
+
+        public override void ExecuteResult(ControllerContext context) {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            if (JsonRequestBehavior == JsonRequestBehavior.DenyGet
+                && String.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase)) {
+                throw new InvalidOperationException("This request has been blocked because sensitive information could be disclosed to third party web sites when this is used in a GET request. To allow GET requests, set JsonRequestBehavior to AllowGet.");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            response.ContentType = !string.IsNullOrEmpty(ContentType)
+                                        ? ContentType
+                                        : "application/json";
+
+            if (ContentEncoding != null)
+                response.ContentEncoding = ContentEncoding;
+
+            if (Data != null) {
+                var writer = new JsonTextWriter(response.Output) { Formatting = Formatting };
+
+                var serializer = JsonSerializer.Create(SerializerSettings);
+                serializer.Serialize(writer, Data);
+
+                writer.Flush();
+            }
+        }
+    }
+}
