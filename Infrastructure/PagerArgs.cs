ade074090b4888ac5c171d9fbbf284399cec8eaf
zicjin <zicjin@gmail.com>
2013-05-29 18:17:46 +0800

    .

---
+++ Infrastructure/PagerArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_net.Infrastructure {
+    public class PagerArgs {
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
