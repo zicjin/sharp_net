7802df58b92f0d86178e4afe215082b879a22301
zicjin <zicjin@gmail.com>
2013-07-29 00:36:59 +0800

    .

--- Mongo/MEntity.cs
+++ Mongo/MEntity.cs
@@ -4,18 +4,8 @@
     using System;
     using System.Runtime.Serialization;
 
-    /// <summary>
-    /// Abstract Entity for all the BusinessEntities.
-    /// </summary>
-    [DataContract]
-    [Serializable]
     [BsonIgnoreExtraElements(Inherited = true)]
     public abstract class MEntity : IMEntity {
-        /// <summary>
-        /// Gets or sets the id for this object (the primary record for an entity).
-        /// </summary>
-        /// <value>The id for this object (the primary record for an entity).</value>
-        [DataMember]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
     }
