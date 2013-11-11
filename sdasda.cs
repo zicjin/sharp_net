36b77d25efae8500ba581759d8150b405402f342
zicjin <zicjin@gmail.com>
2013-11-12 00:47:29 +0800

    .

---
+++ sdasda.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace sharp_net {
+
+    public class WebRequestRobot {
+        const string userAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36";
+        public void DownloadImage(string uri, string filePath) {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Check that the remote file was found. The ContentType
+            // check is performed since a request for a non-existent
+            // image file might be redirected to a 404-page, which would
+            // yield the StatusCode "OK", even though the image was not
+            // found.
+            if (response.StatusCode == HttpStatusCode.OK ||
+                response.StatusCode == HttpStatusCode.Moved ||
+                response.StatusCode == HttpStatusCode.Redirect) {
+                // 抽屉的bug http://img1.chouti.com/group5/M03/74/6C/wKgCEVJXEiDoGigtAA7ki98-vN4272.jpg
+                // response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase)
+
+                // if the remote file was found, download oit
+                string fileName = Path.GetFileName(filePath);
+                string path = filePath.Replace(fileName, "");
+                if (!Directory.Exists(path)) {
+                    Directory.CreateDirectory(path);
+                }
+                using (Stream inputStream = response.GetResponseStream())
+                using (Stream outputStream = File.OpenWrite(filePath)) {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    do {
+                        bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+                        outputStream.Write(buffer, 0, bytesRead);
+                    } while (bytesRead != 0);
+                }
+            }
+        }
+
+        public async Task<string> WeiboShortUrl(string url) {
+            var wsuRequest = WebRequest.Create("http://weibo.com/aj/mblog/video?_wv=5&url=" + url) as HttpWebRequest;
+            if (wsuRequest == null) return string.Empty;// ignore file
+            wsuRequest.UserAgent = userAgent;
+            wsuRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            try {
+                using (var wsuResponse = await wsuRequest.GetResponseAsync() as HttpWebResponse) {
+                    if (wsuResponse.StatusCode != HttpStatusCode.OK)
+                        return string.Empty;
+                    string responseData = new StreamReader(wsuResponse.GetResponseStream()).ReadToEnd();
+                    var wsuObj = JsonConvert.DeserializeObject<WeiboShartUrlResponse>(responseData);
+                    return wsuObj.data;
+                }
+            } catch (Exception) {
+                return string.Empty;
+            }
+        }
+
+        //http://www.cnblogs.com/e241138/archive/2012/12/16/2820054.html
+        public async Task<string> AnalysisVideoUrl(string url) {
+            Task<string> weiboShortUrl = WeiboShortUrl(url);
+            if (string.IsNullOrEmpty(weiboShortUrl.Result))
+                return string.Empty;
+
+            string videoUrl = string.Format("http://api.weibo.com/widget/show.jsonp?vers=3&lang=zh-cn&short_url={0}&template_name=embed&source=2292547934", weiboShortUrl);
+            var videoRequest = WebRequest.Create(videoUrl) as HttpWebRequest;
+            if (videoRequest == null) return string.Empty;// ignore file
+
+            try {
+                using (var videoResponse = await videoRequest.GetResponseAsync() as HttpWebResponse) {
+                    if (videoResponse.StatusCode != HttpStatusCode.OK) return string.Empty;
+                    string responseData = new StreamReader(videoResponse.GetResponseStream()).ReadToEnd();
+                    var videoObj = JsonConvert.DeserializeObject<WeiboVideoResponse>(responseData);
+                    return videoObj.result;
+                }
+            } catch (WebException) {
+                return string.Empty;
+            }
+        }
+
+        public async Task<string> GetWebPageTitle(string url) {
+            var request = HttpWebRequest.Create(url) as HttpWebRequest;
+
+            // If the request wasn't an HTTP request (like a file), ignore it
+            if (request == null) return string.Empty;
+
+            // Use the user's credentials
+            request.UseDefaultCredentials = true;
+
+            // Obtain a response from the server, if there was an error, return nothing
+            try {
+                using (var response = await request.GetResponseAsync() as HttpWebResponse) {
+                    if (new List<string>(response.Headers.AllKeys).Contains("Content-Type")) {
+                        if (response.Headers["Content-Type"].StartsWith("text/html")) {
+                            string responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                            Regex regex = new Regex(@"(?<=<title.*>)([\s\S]*)(?=</title>)", RegexOptions.IgnoreCase);
+                            return regex.Match(responseData).Value.Trim();
+                        }
+                    }
+                }
+            } catch (WebException) {
+                return string.Empty;
+            }
+            return string.Empty;
+        }
+
+        private class WeiboShartUrlResponse {
+            public string code { get; set; }
+            public string msg { get; set; }
+            public string data { get; set; }
+            public string title { get; set; }
+        }
+
+        private class WeiboVideoResponse {
+            public string result { get; set; }
+        }
+    }
+
+}
