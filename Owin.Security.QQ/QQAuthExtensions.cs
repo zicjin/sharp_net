aa46da95e51714aa2c3ab6f90f22604fd9c866e2
cheney jin <zicjin@gmail.com>
2014-04-15 21:26:40 +0800

    .

---
+++ Owin.Security.QQ/QQAuthExtensions.cs
@@ -0,0 +1,21 @@
+namespace Owin.Security.QQ {
+    using System;
+
+    public static class QQAuthExtensions {
+        public static IAppBuilder UseQQAuthentication(this IAppBuilder app, QQAuthenticationOptions options) {
+            if (app == null)
+                throw new ArgumentNullException("app");
+            if (options == null)
+                throw new ArgumentNullException("options");
+            app.Use(typeof(QQAuthenticationMiddleware), app, options);
+            return app;
+        }
+
+        public static IAppBuilder UseQQAuthentication(this IAppBuilder app, string appId, string appKey) {
+            return app.UseQQAuthentication(new QQAuthenticationOptions {
+                AppID = appId,
+                AppKey = appKey,
+            });
+        }
+    }
+}
---
+++ Owin.Security.QQ/QQAuthenticationOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace Owin.Security.QQ {
+    using Microsoft.Owin.Security;
+
+    public class QQAuthenticationOptions : AuthenticationOptions {
+        public ISecureDataFormat<AuthenticationProperties> StateDataFormat { get; set; }
+        public string AppID { get; set; }
+
+        public string AppKey { get; set; }
+
+        public string CallbackPath { get; set; }
+
+        public string Host { get; set; }
+
+        public string SignInAsAuthenticationType { get; set; }
+
+        public IQQAuthenticationProvider Provider { get; set; }
+
+        public QQAuthenticationOptions()
+            : this("QQ") {
+
+        }
+
+        public QQAuthenticationOptions(string authenticationType)
+            : base(authenticationType) {
+            this.Description.Caption = "QQ User";
+            CallbackPath = "/signin-QQ";
+        }
+    }
+}
