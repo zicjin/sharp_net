2260397027335efea3382ee715e3bb26416cc5f6
cheney jin <zicjin@gmail.com>
2014-04-27 15:04:51 +0800

    .

---
+++ Infrastructure/ExpendDateTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using sharp_net.Mvc;
+
+namespace sharp_net {
+
+    public static class ExpendDateTime {
+
+        public static DateTime OffsetCN(this DateTime input) {
+            return input.AddHours(8);
+        }
+
+    }
+}
